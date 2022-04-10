5fd5b3d79922faf856a6514eb87fd49ddcb440b2
Skarph <theSkarph@gmail.com>
2022-04-09 22:55:29 -0400

    Custom colors (#19)

---
+++ Assets/Scripts/CustomColors.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomColors {
+    //Overalls colors
+    public static Dictionary<string, Color> Primary = new() {
+        {"Default",new Color(0f,0f,0f,0f)},
+        {"Rossi",new Color(0.565f,0.322f,0.322f,1f)},
+        {"Wood",new Color(0.541f,0.392f,0.302f,1f)},
+        {"Cashmere",new Color(0.522f,0.478f,0.282f,1f)},
+        {"Shamrock",new Color(0.322f,0.541f,0.376f,1f)},
+        {"Azurite",new Color(0.290f,0.533f,0.486f,1f)},
+        {"Cerulean",new Color(0.322f,0.412f,0.561f,1f)},
+        {"Denim",new Color(0.235f,0.227f,0.337f,1f)},
+        {"Mallow",new Color(0.604f,0.463f,0.561f,1f)},
+        {"Tyrian",new Color(0.510f,0.322f,0.561f,1f)},
+        {"Coal",new Color(0.149f,0.149f,0.149f,1f)},
+    };
+    //Shirt/Hat colors
+    public static Dictionary<string, Color> Secondary = new() {
+        {"Default",new Color(0f,0f,0f,0f)},
+        {"Lobster",new Color(0.659f,0.294f,0.184f,1f)},
+        {"Copper",new Color(0.827f,0.541f,0.165f,1f)},
+        {"Dandelion",new Color(0.859f,0.722f,0.169f,1f)},
+        {"Meadow",new Color(0.549f,0.686f,0.376f,1f)},
+        {"Turqouise",new Color(0.165f,0.690f,0.592f,1f)},
+        {"Sky",new Color(0.278f,0.478f,0.808f,1f)},
+        {"Indigo",new Color(0.322f,0.278f,0.800f,1f)},
+        {"Lilac",new Color(0.690f,0.282f,0.808f,1f)},
+        {"Byzantium",new Color(0.416f,0.137f,0.494f,1f)},
+        {"Coal",new Color(0.376f,0.376f,0.376f,1f)},
+    };
+    
+}
