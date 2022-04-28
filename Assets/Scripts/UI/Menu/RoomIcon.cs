528633f8fdb32e691d73074027042a962a33cd0b
ipodtouch0218 <ipodtouch0218@gmail.com>
2022-04-27 23:28:19 -0400

    lobby symbols

--- Assets/Scripts/UI/Menu/RoomIcon.cs
+++ Assets/Scripts/UI/Menu/RoomIcon.cs
@@ -10,7 +10,7 @@ public class RoomIcon : MonoBehaviour {
     public RoomInfo room;
     public Color defaultColor, highlightColor, selectedColor;
 
-    [SerializeField] TMP_Text playersText, nameText, inProgressText;
+    [SerializeField] TMP_Text playersText, nameText, inProgressText, symbolsText;
     void Start() {
         icon = GetComponent<Image>();
         Unselect();
@@ -22,6 +22,24 @@ public class RoomIcon : MonoBehaviour {
         nameText.text = $"{prop[Enums.NetRoomProperties.HostName]}'s Lobby";
         playersText.text = $"Players: {room.PlayerCount}/{room.MaxPlayers}";
         inProgressText.text = (bool) prop[Enums.NetRoomProperties.GameStarted] ? "In Progress" : "Not Started";
+
+        string symbols = "";
+        bool lives = ((int) prop[Enums.NetRoomProperties.Lives]) >= 1;
+        bool powerups = (bool) prop[Enums.NetRoomProperties.NewPowerups];
+        bool time = ((int) prop[Enums.NetRoomProperties.Time]) >= 1;
+        bool password = ((string) prop[Enums.NetRoomProperties.Password]) != "";
+
+        if (lives)
+            symbols += " <sprite=9>";
+        if (powerups)
+            symbols += " <sprite=8>";
+        if (time)
+            symbols += " <sprite=6>";
+        if (password)
+            symbols += " <sprite=7>";
+
+
+        symbolsText.text = symbols;
     }
     public void Select() {
         icon.color = selectedColor;
