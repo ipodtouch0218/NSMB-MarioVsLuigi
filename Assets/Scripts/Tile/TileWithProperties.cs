db2c11ef62d81d44d8aabe74d7d7b59c4d5c7759
ipodtouch0218 <ipodtouch0218@gmail.com>
2022-06-27 23:24:00 -0400

    v1.6.4.0b Hotfix

--- Assets/Scripts/Tile/TileWithProperties.cs
+++ Assets/Scripts/Tile/TileWithProperties.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "TileWithProperties", menuName = "ScriptableObjects/Tiles/TileWithProperties", order = 3)]
