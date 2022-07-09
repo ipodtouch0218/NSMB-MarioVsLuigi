e344a0f93bbb2a5022559999bc04d0330d2c4725
Brian Campagna <gregh203s@gmail.com>
2022-07-09 02:01:56 -0500

    Mobile Web

--- Assets/Scripts/UI/MobileUIHider.cs
+++ Assets/Scripts/UI/MobileUIHider.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+#if !UNITY_EDITOR && UNITY_WEBGL
+using System.Runtime.InteropServices;
+#endif
 
 public class MobileUIHider : MonoBehaviour
 {
     void Start()
     {
+#if !UNITY_EDITOR && UNITY_WEBGL
+        if (MobileCheck())
+            return;
+#endif
         switch (Application.platform)
         {
             case RuntimePlatform.IPhonePlayer:
@@ -14,4 +21,9 @@ public class MobileUIHider : MonoBehaviour
                 break;
         }
     }
+
+#if !UNITY_EDITOR && UNITY_WEBGL
+    [DllImport("__Internal")]
+    private static extern bool MobileCheck();
+#endif
 }
