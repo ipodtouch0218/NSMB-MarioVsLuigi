643abac9a059e3c58f84076e18f694fb938776ac
ipodtouch0218 <ipodtouch0218@gmail.com>
2022-09-13 18:35:13 -0400

    v1.7.1.0-beta rc1

--- Assets/Scripts/Entity/Enemy/KoopaWalk.cs
+++ Assets/Scripts/Entity/Enemy/KoopaWalk.cs
@@ -282,7 +282,9 @@ public class KoopaWalk : HoldableEntity {
         stationary = crouch;
         currentSpeed = kickSpeed + 1.5f * (Mathf.Abs(holder.body.velocity.x) / holder.RunningMaxSpeed);
         body.position = pos;
-        if (Utils.IsAnyTileSolidBetweenWorldBox(body.position, hitbox.size))
+
+        Debug.DrawLine(body.position + hitbox.offset - hitbox.size / 2f, body.position + hitbox.offset + hitbox.size / 2f, Color.white, 10f);
+        if (Utils.IsAnyTileSolidBetweenWorldBox(body.position + hitbox.offset, hitbox.size))
             transform.position = body.position = new Vector2(holder.transform.position.x, transform.position.y);
 
         previousHolder = holder;
