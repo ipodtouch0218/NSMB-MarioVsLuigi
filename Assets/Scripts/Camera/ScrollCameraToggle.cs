2361e5c1885c6e06eb171961f5ee4ca68be40457
Jack Stormont <stormont.jack@gmail.com>
2023-03-20 00:22:29 -0400

    ScrollCamera RenderToggle
    Created a Script so that when invisible objects are in frame on either the Left or Right Hand sides of the stage, then the Scroll Camera will render.
    
    Otherwise, the Camera will be disabled, saving on performance (in theory, have yet to test outside of the Unity Editor).
    
    Furthermore, this is also exclusively on Grasslands at the moment, but the setup is simple enough to implemented on all looping stages.

---
+++ Assets/Scripts/Camera/ScrollCameraToggle.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class ScrollCameraToggle : MonoBehaviour
+{
+    //The scroll camera being stored
+    public Camera scrollCamera;
+
+    //Invisible Renderer only for checking purposes (set to Sprite Renderer of the Object)
+    public Renderer renderCheck;
+
+    //Other ScrollCameraToggleGameObject
+    public ScrollCameraToggle otherScrollChecker;
+    
+    void Start()
+    {
+        //At the start, assign the Base & Main Cameras.
+        scrollCamera = GameObject.Find("ScrollCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If the object is not visible, then disable the ScrollCamera Camera component
+		if (renderCheck.isVisible == false && otherScrollChecker.renderCheck.isVisible == false)
+		{
+			if (scrollCamera.isActiveAndEnabled)
+			{
+                Debug.Log("Scroll Camera is Disabled");
+                scrollCamera.enabled = false;
+            }
+            return;
+		}
+
+		if (scrollCamera.isActiveAndEnabled == false)
+		{
+            Debug.Log("Scroll Camera is Re-Enabled");
+            scrollCamera.enabled = true;
+		}
+    }
+}
