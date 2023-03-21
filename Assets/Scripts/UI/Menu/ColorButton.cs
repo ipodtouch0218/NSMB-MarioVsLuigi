392c6eb5893d0217ad378b309db6ad04dcd295c8
Cubby <112747147+CubbyCrazes@users.noreply.github.com>
2023-03-20 22:28:18 -0400

    Color name bar (Like from Cubbert Cra Mod (Real, perhaps?)) (#177)
    I hope you know I had to redo this twice because of GitHub being stupid

--- Assets/Scripts/UI/Menu/ColorButton.cs
+++ Assets/Scripts/UI/Menu/ColorButton.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
 
 public class ColorButton : MonoBehaviour, ISelectHandler, IDeselectHandler {
 
     [SerializeField] private Sprite overlayUnpressed, overlayPressed;
     [SerializeField] private Image shirt, overalls, overlay;
+    public GameObject ColorNameString;
 
     public PlayerColorSet palette;
 
@@ -25,6 +27,7 @@ public class ColorButton : MonoBehaviour, ISelectHandler, IDeselectHandler {
     public void OnSelect(BaseEventData eventData) {
         overlay.enabled = true;
         overlay.sprite = overlayUnpressed;
+        ColorNameString.GetComponent<TMP_Text>().text = palette.ColorText;
     }
 
     public void OnDeselect(BaseEventData eventData) {
@@ -34,5 +37,6 @@ public class ColorButton : MonoBehaviour, ISelectHandler, IDeselectHandler {
 
     public void OnPress() {
         overlay.sprite = overlayPressed;
+        ColorNameString.GetComponent<TMP_Text>().text = palette.ColorText;
     }
 }
